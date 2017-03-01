88e620c56773f2d9b2fa05f67c6061b4d566d57c
Minolalaina <minolalainarakoto@gmail.com>
2017-03-01 14:01:37 +0100

    initial value

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>YouSEE</title>
+		<link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="./assets/css/font-awesome.min.css">
+		<link rel="stylesheet" href="./assets/css/style.css" >
+	</head>
+	<body>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-3 block1 text-center">
+					<i class="fa fa-home fa-4x">  Home</i>
+				</div>
+				<div class="col-md-3 block2 text-center">
+					<i class="fa fa-video-camera fa-4x">  Video</i>
+				</div>
+				<div class="col-md-3 block3 text-center">
+					<i class="fa fa-music fa-4x">  Music</i>
+				</div>
+				<div class="col-md-3 block4 text-center">
+					<i class="fa fa-envelope fa-4x">Message</i>
+				</div>
+			</div>
+			<div class="search">
+				<div class="col-md-2 task">
+					<ul>
+						<br>
+						<li><i class="fa fa-music fa-2x"></i>  MUSIQUE</li>
+						<br>
+						<li><i class="fa fa-video-camera fa-2x"></i>  FILM</li>
+						<br>
+						<li><i class="fa fa-newspaper-o fa-2x"></i>  DOCUMENTAIRE</li>
+						<br>
+						<li><i class="fa fa-bicycle fa-2x"></i>  DIVERTISSEMENT</li>
+					</ul>
+				</div>
+				<div class="col-md-8 task2">
+					video
+				<div>
+			</div>
+		</div>
+	<script src="./assets/js/jquery.min.js"></script>
+	<script src="./assets/js/bootstrap.min.js"></script>
+	</body>	
+</html>
+
