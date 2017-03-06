c8c79ae2b82175b9b79abd3f1904cb17c3fc79c3
Minolalaina <minolalainarakoto@gmail.com>
2017-03-06 13:16:50 +0100

    Second modification of title

--- index.php
+++ index.php
@@ -2,28 +2,13 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>NOW</title>
+		<title>titre de la page</title>
 		<link rel="stylesheet" href="./assets/css/bootstrap.min.css">
 		<link rel="stylesheet" href="./assets/css/font-awesome.min.css">
 		<link rel="stylesheet" href="./assets/css/style.css" >
 	</head>
 	<body>
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-md-3 block1 text-center">
-					<i class="fa fa-home fa-4x">  Home</i>
-				</div>
-				<div class="col-md-3 block2 text-center">
-					<i class="fa fa-video-camera fa-4x">  Video</i>
-				</div>
-				<div class="col-md-3 block3 text-center">
-					<i class="fa fa-music fa-4x">  Music</i>
-				</div>
-				<div class="col-md-3 block4 text-center">
-					<i class="fa fa-envelope fa-4x">Message</i>
-				</div>
-			</div>
-		</div>
+                   
 	<script src="./assets/js/jquery.min.js"></script>
 	<script src="./assets/js/bootstrap.min.js"></script>
 	</body>	
